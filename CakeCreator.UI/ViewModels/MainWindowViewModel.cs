336135e89fdcf627adf9cb08a4aa7ed475b7a000
Natalia Krzus <nkrzus95@gmail.com>
2025-04-11 20:32:39 +0200

    Create UI project

---
+++ CakeCreator.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace CakeCreator.UI.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ CakeCreator.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CakeCreator.UI.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}

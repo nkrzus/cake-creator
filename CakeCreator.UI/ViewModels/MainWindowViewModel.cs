a72aa7bb888684a3e84465b3c65128a858c71e28
Natalia Krzus <nkrzus95@gmail.com>
2025-05-10 15:55:42 +0200

    Add Base recipes view and builders

--- CakeCreator.UI/ViewModels/MainWindowViewModel.cs
+++ CakeCreator.UI/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,21 @@
-﻿namespace CakeCreator.UI.ViewModels
+﻿using System.Collections.ObjectModel;
+
+namespace CakeCreator.UI.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        public string Greeting { get; } = "Welcome to CakeCreator! ";
+
+        public ObservableCollection<string> Items { get; set; } = new ObservableCollection<string>();
+
+        public string SelectedItem { get; set; }
+
+        public MainWindowViewModel()
+        {
+            for (int i = 1; i <= 50; i++)
+            {
+                Items.Add($"Element {i}");
+            }
+        }
     }
 }
--- CakeCreator.UI/Views/MainWindow.axaml.cs
+++ CakeCreator.UI/Views/MainWindow.axaml.cs
@@ -1,12 +1,32 @@
 using Avalonia.Controls;
-
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 namespace CakeCreator.UI.Views
 {
     public partial class MainWindow : Window
     {
+        public ObservableCollection<string> Items { get; set; } = new ObservableCollection<string>();
+        public string SelectedItem { get; set; }
         public MainWindow()
         {
+            //InitializeComponent();
+            // cakeIngredients.ItemsSource = new string[]
+            //      {"cat", "camel", "cow", "chameleon", "mouse", "lion", "zebra" }
+            //    .OrderBy(x => x);
+
+
             InitializeComponent();
+            DataContext = this;
+
+            // Dodajemy przyk³adowe dane
+           
+        }
+
+        private void Button_OnClick(object? sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Click!");
         }
     }
 }
